1b2b2bfd68343873ae7a84091480e04cee5c9af0
Nick Couchman <nick.couchman@os3llc.com>
2017-11-09 14:04:47 -0500

    Documentation, style fixes, and some minor code tweaks.

--- example.php
+++ example.php
@@ -22,35 +22,50 @@ use Sabre\DAV;
 require 'vendor/autoload.php';
 require_once 'include/sftpdav.php';
 
+// Create the SSH connection.
 $connection = ssh2_connect('sftp.example.com', 22);
 
+// This example uses built-in HTTP BASIC authentication to
+// get a username and password, and then passes that onto the
+// SSH authentication process.
 if (!isset($_SERVER['PHP_AUTH_USER'])) {
-	header('WWW-Authenticate: Basic realm="SFTP Server"');
-	header('HTTP/1.0 401 Unauthorized');
-	echo 'Please log in with your SFTP Server Credentials.';
-	exit;
+    header('WWW-Authenticate: Basic realm="SFTP Server"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'Please log in with your SFTP Server Credentials.';
+    exit;
 }
 else {
-	if (!ssh2_auth_password($connection, $_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'])) {
-		header('WWW-Authenticate: Basic realm="SFTP Server"');
-		header('HTTP/1.0 401 Unauthorized');
-		echo 'SFTP Server login failed, please try, again.';
-		exit;
-	}
+    if (!ssh2_auth_password($connection, $_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'])) {
+        header('WWW-Authenticate: Basic realm="SFTP Server"');
+        header('HTTP/1.0 401 Unauthorized');
+        echo 'SFTP Server login failed, please try, again.';
+        exit;
+    }
 
-	$sftp = ssh2_sftp($connection);
+    // Create the SFTP connection from the successful SSH authentication.
+    $sftp = ssh2_sftp($connection);
 
-	$dir =  new SFTPDirectory('/.', $sftp);
+    // Create the base SFTPDirectory object.  Note that if you're starting at
+    // "/", you'll need the "/." work-around - else you'll get errors trying to
+    // get the contents of the root directory.  This includes situations where
+    // your are jailing SFTP users into their own home directories.
+    $dir =  new SFTPDirectory('/.', $sftp);
 
-	$server = new DAV\Server($dir);
+    // Create the WebDAV server.
+    $server = new DAV\Server($dir);
 
-	$server->setBaseUri('/sftp/index.php/');
+    // Sets the Base URI of the WebDAV instance. Adjust based on where you're installing things.
+    $server->setBaseUri('/sftp/example.php/');
 
-	$lockBackend = new DAV\Locks\Backend\File('/tmp/dav.locks');
-	$server->addPlugin(new DAV\Locks\Plugin($lockBackend));
-	$server->addPlugin(new DAV\Browser\Plugin());
+    // Initializes the locking backend.
+    $lockBackend = new DAV\Locks\Backend\File('/tmp/dav.locks');
+    $server->addPlugin(new DAV\Locks\Plugin($lockBackend));
 
-	$server->exec();
+    // Provides a basic web-based DAV client so that browsers can see the contents.
+    $server->addPlugin(new DAV\Browser\Plugin());
+
+    // Go run it.
+    $server->exec();
 }
 
 ?>
--- include/sftpdav.php
+++ include/sftpdav.php
@@ -21,147 +21,199 @@ limitations under the License.
 
 use Sabre\DAV;
 
+// Class that extends the DAV\Collection class to provide a SFTP-
+// specific implementation of the DAV Collection.
 class SFTPDirectory extends DAV\Collection {
 
-	private $myPath;
-	private $mySSH;
+    // The path for this SFTPDirectory object.
+    private $myPath;
 
-	function __construct($myPath, $mySSH) {
-		$this->myPath = $myPath;
-		$this->mySSH = $mySSH;
-	}
+    // The PECL SSH2 SFTP connection object for this
+    // SFTPDirectory object.
+    private $mySSH;
 
-	function getChildren() {
-		$fdPath = 'ssh2.sftp://' . intval($this->mySSH) . $this->myPath;
-		$children = array();
+    // Constructor for the class, taking in a path (directory)
+    // and a PECL SSH2 SFTP connection object.
+    function __construct($myPath, $mySSH) {
+        $this->myPath = $myPath;
+        $this->mySSH = $mySSH;
+    }
 
-		$contents = scandir($fdPath);
-		if ($contents === false)
-			throw new DAV\Exception\NotFound('Access denied.');
-		foreach ($contents as $node) {
-			if ($node[0] === '.')
-				continue;
-			$children[] = $this->getChild($node);
-		}
+    // Returns an array of child objects of the current directory
+    // recursing into each child and generating the appropriate
+    // object type for each.
+    function getChildren() {
+        $fdPath = 'ssh2.sftp://' . intval($this->mySSH) . $this->myPath;
+        $children = array();
 
-		return $children;
+        $contents = scandir($fdPath);
+        if ($contents === false)
+            throw new DAV\Exception\NotFound('Access denied.');
+        foreach ($contents as $node) {
+            // Skip hidden files and directory markers
+            if ($node[0] === '.')
+                continue;
+            $children[] = $this->getChild($node);
+        }
 
-	}
+        return $children;
 
-	function getChild($name) {
-		$path = $this->myPath . '/' . $name;
-		$fd_path = 'ssh2.sftp://' . intval($this->mySSH) . $path;
+    }
 
-		if (!file_exists($fd_path))
-			throw new DAV\Exception\NotFound('The file ' . $name . ' is not present on the SFTP server, ' . $fd_path);
+    // Get the named child and return the appropriate object
+    // type for the entry, either a SFTPDirectory or SFTPFile.
+    function getChild($name) {
+        $path = $this->myPath . '/' . $name;
+        $fd_path = 'ssh2.sftp://' . intval($this->mySSH) . $path;
 
-		if ($name[0] == '.')
-			throw new DAV\Exception\NotFound('Access denied.');
+        // If the file does not exist, throw an exception.
+        if (!file_exists($fd_path))
+            throw new DAV\Exception\NotFound('The file ' . $name . ' is not present on the SFTP server, ' . $fd_path);
 
-		if (is_dir($fd_path))
-			return new SFTPDirectory($path, $this->mySSH);
+        // Do not provide access to hidden files and directory markers.
+        if ($name[0] == '.')
+            throw new DAV\Exception\NotFound('Access denied.');
 
-		else
-			return new SFTPFile($path, $this->mySSH);
+        // If the entry is a directory, return a new SFTPDirectory object.
+        if (is_dir($fd_path))
+            return new SFTPDirectory($path, $this->mySSH);
 
-	}
+        // Otherwise return a SFTPFile object.
+        else
+            return new SFTPFile($path, $this->mySSH);
 
-	function childExists($name) {
+    }
 
-		if (!ssh2_sftp_stat($this->mySSH, $this->myPath . "/" . $name))
-			return false;
+    // Checks to see if the named object exists.
+    function childExists($name) {
 
-		return true;
+        if (!ssh2_sftp_stat($this->mySSH, $this->myPath . "/" . $name))
+            return false;
 
-	}
+        return true;
 
-	function getName() {
+    }
 
-		return basename($this->myPath);
+    // Get the base name of the current object.
+    function getName() {
 
-	}
+        return basename($this->myPath);
 
-	function delete() {
-		return ssh2_sftp_rmdir($this->mySSH, $this->myPath);
-	}
+    }
 
-	function setName($newName) {
-		return ssh2_sftp_rename($this->mySSH, $this->myPath, dirname($this->myPath) . '/' . $newName);
-	}
+    // Delete this object.
+    function delete() {
+        return ssh2_sftp_rmdir($this->mySSH, $this->myPath);
+    }
 
-	function createDirectory($name) {
-		$newPath = $this->myPath . '/' . $name;
-		if(ssh2_sftp_mkdir($this->mySSH, $newPath))
-			return new SFTPDirectory($newPath);
-		return false;
-	}
+    // Set the name of this object.
+    function setName($newName) {
+        return ssh2_sftp_rename($this->mySSH, $this->myPath, dirname($this->myPath) . '/' . $newName);
+    }
 
-	function createFile($name, $data = NULL) {
-		$filePath = $this->myPath . '/' . $name;
-		$fd_filePath = 'ssh2.sftp://' . intval($this->mySSH) . '/' . $filePath;
-		if (file_exists($fd_filePath))
-			return false;
-		$fileStream = fopen($fd_filePath, 'w');
-		if(fwrite($fileStream, $data) === true) {
-			fclose($fileStream);
-			return new SFTPFile($this->mySSH, $filePath);
-		}
-		return false;
-	}
+    // Create a new directory under this object, and return the
+    // SFTPDirectory object to that new directory.
+    function createDirectory($name) {
+        $newPath = $this->myPath . '/' . $name;
+        if(ssh2_sftp_mkdir($this->mySSH, $newPath))
+            return new SFTPDirectory($newPath);
+        return false;
+    }
+
+    // Create a new file under this object, and return the
+    // SFTPFile object for that file.  The first parameter is the
+    // name of the file, the second is the data to place into the
+    // file and is optional.
+    function createFile($name, $data = NULL) {
+        $filePath = $this->myPath . '/' . $name;
+        $fd_filePath = 'ssh2.sftp://' . intval($this->mySSH) . '/' . $filePath;
+
+        // If the file already exsists, we bail out.
+        if (file_exists($fd_filePath))
+            return false;
+        $fileStream = fopen($fd_filePath, 'w');
+        $retval = stream_copy_to_stream($data, $fileStream);
+
+        // If the operation was successful, return the SFTPFile
+        // object associated with the new file.
+        if ($retval !== false) {
+            fclose($fileStream);
+            fclose($data);
+            return new SFTPFile($this->mySSH, $filePath);
+        }
+        return false;
+    }
 
 }
 
+// A class that extends DAV\File to implement SSH/SFTP-specific
+// handlers for files located on a SFTP server.
 class SFTPFile extends DAV\File {
 
-	private $myPath;
-	private $mySSH;
+    // The path to the current file.
+    private $myPath;
 
-	function __construct($myPath, $mySSH) {
-		$this->myPath = $myPath;
-		$this->mySSH = $mySSH;
-	}
+    // The PECL SSH2 SFTP connection object.
+    private $mySSH;
 
-	function getName() {
-		return basename($this->myPath);
-	}
+    // Construct for the class, takes the full path to the file
+    // and the PECL SSH2 SFTP connection object.
+    function __construct($myPath, $mySSH) {
+        $this->myPath = $myPath;
+        $this->mySSH = $mySSH;
+    }
 
-	function get() {
-		$fdPath = 'ssh2.sftp://' . intval($this->mySSH) . '/' . $this->myPath;
-		return fopen($fdPath, 'r');
-	}
+    // Return the name of the object, without the path.
+    function getName() {
+        return basename($this->myPath);
+    }
 
-	function getSize() {
-		$fdPath = 'ssh2.sftp://' . intval($this->mySSH) . '/' . $this->myPath;
-		return filesize($fdPath);
-	}
+    // Return an open stream for the file.
+    function get() {
+        $fdPath = 'ssh2.sftp://' . intval($this->mySSH) . '/' . $this->myPath;
+        return fopen($fdPath, 'r');
+    }
 
-	function getETag() {
-		$fdPath = 'ssh2.sftp://' . intval($this->mySSH) . '/' . $this->myPath;
-		return '"' . md5_file($fdPath) . '"';
-	}
+    // Return the size, in bytes, of the file.
+    function getSize() {
+        $fdPath = 'ssh2.sftp://' . intval($this->mySSH) . '/' . $this->myPath;
+        return filesize($fdPath);
+    }
 
-	function put($data) {
-                $fdPath = 'ssh2.sftp://' . intval($this->mySSH) . '/' . $this->myPath;
-                if (!file_exists($fdPath))
-                        return false;
-                $fileStream = fopen($fdPath, 'w');
-		$retval = stream_copy_to_stream($data, $fileStream);
-		if($retval) {
-			fclose($fileStream);
-			fclose($data);
-			return true;
-		}
-                return false;
-	}
+    // Return the MD5 SUM of the file.
+    function getETag() {
+        $fdPath = 'ssh2.sftp://' . intval($this->mySSH) . '/' . $this->myPath;
+        return '"' . md5_file($fdPath) . '"';
+    }
 
-	function getContentType() {
-		$fdPath = 'ssh2.sftp://' . intval($this->mySSH) . '/' . $this->myPath;
-		return mime_content_type($fdPath);
-	}
+    // Update data in the file, replacing the contents with the data specified.
+    function put($data) {
+        $fdPath = 'ssh2.sftp://' . intval($this->mySSH) . '/' . $this->myPath;
 
-	function delete() {
-		return ssh2_sftp_unlink($this->mySSH, $this->myPath);
-	}
+        // If the file does not exist, we cannot update it.
+        if (!file_exists($fdPath))
+            return false;
+
+        $fileStream = fopen($fdPath, 'w');
+        $retval = stream_copy_to_stream($data, $fileStream);
+        if ($retval) {
+            fclose($fileStream);
+            fclose($data);
+            return true;
+        }
+        return false;
+    }
+
+    // Return the MIME content type of the file.
+    function getContentType() {
+        $fdPath = 'ssh2.sftp://' . intval($this->mySSH) . '/' . $this->myPath;
+        return mime_content_type($fdPath);
+    }
+
+    // Delete this object.
+    function delete() {
+        return ssh2_sftp_unlink($this->mySSH, $this->myPath);
+    }
 
 }
 
